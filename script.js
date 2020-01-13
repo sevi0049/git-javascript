167c529627fc4ab2209f75b9c3fbb284bce6e5ab
Matthew Sevillano <sevi0049@algonquinlive.com>
2020-01-13 15:33:30 -0500

    Adds index.html and script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = 'Matthew'
+console.log(`Hello, my name is ${name} .`)
+
+let number = 5
+let product = number * 2
+console.log(`the number ${number} multiplied by by 2 is ${number * 2} .`)
+
+console.log(`The number multiplied by 5 is ${product * 5}.`)
+console.log(`the number divided by 5 is ${(product * 5) / number}`)
+
+let finalValue = (product * 5) / number
+console.log(`The result minus 7 is ${finalValue - 7}.`)
